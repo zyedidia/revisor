3a6aa2e9e7bcf57949326fa7e0fbbbe4bcae139e
Zachary Yedidia <zyedidia@gmail.com>
2023-12-05 20:37:36 -0800

    Specify mgeneral-regs-only

--- rekernel/build-picolibc.sh
+++ rekernel/build-picolibc.sh
@@ -4,17 +4,17 @@ mkdir -p build
 cd build
 
 meson \
-	-Dtls-model=local-exec \
-	-Dmultilib=false \
-	-Dpicolib=false \
-	-Dpicocrt=false \
-	-Dposix-console=true \
-	-Dnewlib-global-atexit=true \
+    -Dtls-model=local-exec \
+    -Dmultilib=false \
+    -Dpicolib=false \
+    -Dpicocrt=false \
+    -Dposix-console=true \
+    -Dnewlib-global-atexit=true \
     -Dprefix=$PWD/install \
-	-Dincludedir=include \
-	-Dlibdir=lib \
+    -Dincludedir=include \
+    -Dlibdir=lib \
     -Dthread-local-storage=false \
-	-Dspecsdir=none \
+    -Dspecsdir=none \
     ../picolibc
 ninja
 ninja install
